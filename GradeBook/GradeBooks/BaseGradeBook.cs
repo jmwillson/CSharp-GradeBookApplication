0691ef90a10642cb1bc2e67e812a1ff3393e1ef1
James <43877294+jmwillson@users.noreply.github.com>
2019-07-28 21:46:03 +1000

    Creating New Gradebook Types

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -9,11 +9,10 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class BaseGradeBook
-    {
+    public class BaseGradeBook {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
-
+        public GradeBookType Type { get; set; }
         public BaseGradeBook(string name)
         {
             Name = name;
---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,7 @@
+﻿namespace GradeBook.GradeBooks {
+    public class RankedGradeBook : BaseGradeBook {
+        public RankedGradeBook(string name) : base(name) {
+            Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
